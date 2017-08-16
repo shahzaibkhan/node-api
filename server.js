20428ce5feeacb3c190b40c8ba378cf88bd2f531
Shahzaib Khan <shahzaib198@gmail.com>
2017-08-16 22:22:11 +0500

    Update 2

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+
+mongoose = require('mongoose'),
+senseMap = require('./api/models/sensemapModel'),  
+bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://localhost/sensemapDb'); 
+mongoose.connect('mongodb://shahzaib:sVgpQm4SejTdhz9f@ds157682.mlab.com:57682/smapdb'); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var routes = require('./api/routes/sensemapRoutes'); 
+routes(app);
+
+app.listen(port);
+console.log('senseMap RESTful API server started on: ' + port);
